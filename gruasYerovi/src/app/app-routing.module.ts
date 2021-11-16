66fb62889194e2e06d0db7e81c959eaca00c7b9b
Geovanny Rojas <geovanny_rl14@hotmail.com>
2021-11-15 23:26:27 -0500

    Agrego el modulo HttpClientModule

--- gruasYerovi/src/app/app-routing.module.ts
+++ gruasYerovi/src/app/app-routing.module.ts
@@ -8,11 +8,13 @@ import { SeguimientoComponent } from './pages/seguimiento/seguimiento.component'
 import { GruaComponent } from './pages/servicio/grua/grua.component';
 import { MantenimientoComponent } from './pages/servicio/mantenimiento/mantenimiento.component';
 import { ServicioComponent } from './pages/servicio/servicio.component';
+import { PromosComponent } from './pages/promos/promos.component';
 
 const routes: Routes = [
   { path: 'iniciarSesion', component: LoginComponent },
   { path: 'registro', component: RegisterComponent},
   { path: 'noticias', component: NoticiasComponent},
+  { path: 'promo/:id', component: PromosComponent },
   { path: 'seguimiento', component: SeguimientoComponent},
   { path: 'servicio', component: ServicioComponent},
   { path: 'servicio-grua', component:GruaComponent},
