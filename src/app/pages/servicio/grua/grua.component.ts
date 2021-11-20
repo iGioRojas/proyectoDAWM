efd16b8f6b3e06a771f43517798e4fae3c0d6b00
Geovanny Rojas <geovanny_rl14@hotmail.com>
2021-11-19 23:38:37 -0500

    Cambio el css, html y añado el mapa con TS

--- src/app/pages/servicio/grua/grua.component.ts
+++ src/app/pages/servicio/grua/grua.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const L:any;
+
 @Component({
   selector: 'app-grua',
   templateUrl: './grua.component.html',
@@ -7,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GruaComponent implements OnInit {
 
+  title = 'locationApp'
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  localizar(){
+    if(!navigator.geolocation){
+      console.log('location is not supported');
+    }
+    navigator.geolocation.getCurrentPosition((position)=>{
+      const coords = position.coords;
+      const latLong = [coords.latitude,coords.longitude];
+      let mymap = L.map('map').setView(latLong, 14);
+      let marker = L.marker(latLong).addTo(mymap);
+      L.tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'your.mapbox.access.token'
+      }).addTo(mymap);
+    });
+  }
 }
