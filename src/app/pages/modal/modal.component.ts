00b598b49b2789c822b28b2a6afb3a207993d316
Geovanny Rojas <geovanny_rl14@hotmail.com>
2021-12-02 23:40:23 -0500

    Info del modal

--- src/app/pages/modal/modal.component.ts
+++ src/app/pages/modal/modal.component.ts
@@ -6,13 +6,19 @@ import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
   selector: 'app-modal',
   template: `
     <div class="modal-header">
-      <h4 class="modal-title">Hi there!</h4>
-      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
-        <span aria-hidden="true">&times;</span>
+      <h4 class="modal-title">Información adicional</h4>
+      <button type="button" class=" btn btn-outline-dark" (click)="activeModal.dismiss('Cross click')">
+        <span>X</span>
       </button>
     </div>
     <div class="modal-body">
-      <p>Hello, {{name}}!</p>
+      <p>Hola cliente, el mecánico a dicho lo siguiente:</p>
+      <p>{{name}}</p>
+      <p><strong>Fecha y hora: </strong>2/11/2021 - 13:00</p>
+      <p><strong>Mecánico: </strong>Joel Choez</p>
+    </div>
+    <div>
+      <img  class = "w-100 p-2" src="assets/img/amortiguador.jpg" alt="amortiguador">
     </div>
     <div class="modal-footer">
       <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
@@ -30,13 +36,14 @@ export class ModalComponent implements OnInit {
 
 @Component({
   selector: 'ngbd-modal-component',
-  templateUrl: './modal.component.html'
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
 })
 export class NgbdModalComponent {
   constructor(private modalService: NgbModal) {}
 
   open() {
     const modalRef = this.modalService.open(ModalComponent);
-    modalRef.componentInstance.name = 'World';
+    modalRef.componentInstance.name = 'Los amortiguadores estaban muy gastados, patrón.';
   }
 }
