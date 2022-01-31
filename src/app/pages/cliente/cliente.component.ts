c6fdb5b65541b938cd7df9bf0c7f21f0ad4b8d44
Geovanny Rojas Lindao <geovanny_rl14@hotmail.com>
2022-01-31 16:56:56 -0500

    Agregado los guard para cliente, presento la información del cliente

--- src/app/pages/cliente/cliente.component.ts
+++ src/app/pages/cliente/cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/interfaces/usuario';
 
 @Component({
   selector: 'app-cliente',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteComponent implements OnInit {
 
-  constructor() { }
+  user:Usuario;
+  promociones:string[] = []
+  promocion:any;
+  constructor() {
+    this.user = {
+      id_usuario:0,
+      cedula:'',
+      nombres:'',
+      apellidos:'',
+      fecha_nacimiento:'',
+      ubicacion:'',
+      celular:'',
+      foto:{
+        type:'',
+        data:'',
+      },
+      rol:'',
+      correo:''
+    }
+   }
 
   ngOnInit(): void {
+    this.infoCliente();
+    this.mostrarInfo();
   }
+
+    infoCliente(){
+      let token = localStorage.getItem("token");
+      fetch("http://localhost:3001/usuarios/token",{
+        method:'post',
+        headers:{
+          'authorization':`Bearer ${token}`
+        }
+      }).then(response => response.json())
+      .then(data => {
+        let id = data['authData']['id'];
+        fetch(`http://localhost:3001/usuarios/id/${id}`).then(response => response.json())
+        .then(data => {
+          this.user = data;
+          fetch(`http://localhost:3001/usuarios/ubicacion/${this.user.ubicacion}`).then(response => response.json())
+          .then(data => this.user.ubicacion = data['provincia']);
+        });
+      });
+    }
+
+    mostrarInfo(){
+      fetch("http://localhost:3001/promociones").then(response => response.json())
+      .then(data => {
+        this.promociones = data;
+        this.promocion = this.promociones[Math.floor(Math.random() * this.promociones.length)];
+      });
+
+    }
 
 }
