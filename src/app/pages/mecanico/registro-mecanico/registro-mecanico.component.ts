ff5c1fe8afd6907df4388bfcd0521f5298baf367
Geovanny Rojas Lindao <geovanny_rl14@hotmail.com>
2022-02-01 17:19:35 -0500

    Registro completado, revisar el registrar mecanico

--- src/app/pages/mecanico/registro-mecanico/registro-mecanico.component.ts
+++ src/app/pages/mecanico/registro-mecanico/registro-mecanico.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { isGeneratedFile } from '@angular/compiler/src/aot/util';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroMecanicoComponent implements OnInit {
 
-  constructor() { }
+  nombre:string = "";
+  correo:string = "";
+  cedula:string = "";
+  fecha:string = "";
+  contra:string = "";
+  apellido:string = "";
+  celular:string = "";
+  rol:string = "mecanico";
+  error:boolean = false;
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  registrar(){
+    if(this.cedula!="" && this.contra!="" && this.nombre!='' && this.correo!='' && this.fecha!='' && this.contra!=''){
+      let nom = this.nombre.split(" ");
+    if(nom.length >0){
+      this.apellido = nom[1];
+      this.nombre = nom[0];
+    }
+    this.http.put<any>("http://localhost:3001/usuarios/registro",{
+      nombre:this.nombre,
+      correo:this.correo,
+      fecha:this.fecha,
+      cedula:this.cedula,
+      apellido:this.apellido,
+      rol:this.rol,
+      celular:this.celular,
+      contrasenia:this.contra
+    }).subscribe(data => {
+      if(data.status ==400){
+        this.error = true;
+      }
+    })
+    }else{
+      this.error = true;
+    }
+
   }
 
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, ElementRef, OnInit } from '@angular/core';
 
@@ -12,7 +13,19 @@ export class RegisterComponent implements OnInit {
   provincias:string[] = [];
   ciudades:string[] = [];
   valorProvincia:string = "";
-  constructor() { }
+  cedula:string = '';
+  nombres:string = '';
+  apellidos:string = '';
+  fecha_nacimiento:string = '';
+  ciudad:string = '';
+  celular:string = '';
+  rol:string = 'cliente';
+  correo:string = '';
+  contra:string = '';
+  referencia:string = '';
+  error:boolean = false;
+  login:boolean = false;
+  constructor(public http:HttpClient) { }
 
   ngOnInit(): void {
     this.ubicacion();
@@ -42,6 +55,31 @@ export class RegisterComponent implements OnInit {
       }
   }
 
+  registrar(){
+    let validar = this.contra!='' && this.correo !='' && this.nombres !='' && this.apellidos != '' && this.cedula != '' && this.valorProvincia !='' && this.ciudad != '' && this.fecha_nacimiento != '' && this.celular != '';
 
-
+    if(validar){
+      this.http.put<any>("http://localhost:3001/usuarios/registro",{
+      nombre:this.nombres,
+      correo:this.correo,
+      fecha:this.fecha_nacimiento,
+      cedula:this.cedula,
+      apellido:this.apellidos,
+      rol:this.rol,
+      celular:this.celular,
+      contrasenia:this.contra,
+      provincia:this.valorProvincia,
+      ciudad:this.ciudad,
+      referencia:this.referencia,
+      }).subscribe(data => {
+        if(data.status == 400){
+          this.error = true;
+        }else{
+          this.login = true;
+        }
+      })
+    }else{
+      this.error = true;
+    }
+  }
 }
